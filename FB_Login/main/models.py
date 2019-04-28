27f63e472a8ed12be530e420ffb9b122d7a4b744
Islam Bayoumy <islam.bayoumy@gmail.com>
2019-04-28 12:57:36 +0200

    Add: 'UserProfile' Model extending Auth.User

--- FB_Login/main/models.py
+++ FB_Login/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.URLField(null=True)
+    access_token = models.CharField(max_length=255,null=True, blank=True)
+    fb_id = models.IntegerField(null=True)
